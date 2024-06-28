29bf3c55f3702088676a906e9563394a251c612d
winnieaug22 <winnie.aug22@gmail.com>
2024-06-28 11:30:58 +0800

    fpgaInit.sh, myrun: FPGA_PREFIXED -> fpgabit

--- fpgaInit.sh
+++ fpgaInit.sh
@@ -4,7 +4,7 @@
 libf=/lib/firmware
 xilinxbase=$libf/xilinx/base
 blite=$libf/blite
-fpgam=/sys/class/fpga_manager
+fmanager=/sys/class/fpga_manager
 conf=/configfs/device-tree/overlays/full
 home=/home/root
 testfile=image_test_14M.ub
@@ -16,12 +16,12 @@ fi
 echo
 
 echo "---$fpgabit---"
-echo "echo 0 > $fpgam/fpga0/flags"
-echo "echo $fpgabit > $fpgam/fpga0/firmware"
+echo "echo 0 > $fmanager/fpga0/flags"
+echo "echo $fpgabit > $fmanager/fpga0/firmware"
 if [ "$gogo" -eq 1 ]; then
-    echo 0 > $fpgam/fpga0/flags
+    echo 0 > $fmanager/fpga0/flags
     # echo system.bit > /sys/class/fpga_manager/fpga0/firmware
-    echo $fpgabit > $fpgam/fpga0/firmware
+    echo $fpgabit > $fmanager/fpga0/firmware
 fi
 echo
 
@@ -47,10 +47,17 @@ fi
 echo "---Done---"
 echo
 
-echo "---testspw---"
+echo "--- testspw in $libf ---"
 echo "testspw rx"
-echo "cp $blite/$testfile $home"
+echo "cp $blite/$testfile $libf"
 echo "testspw tx $testfile"
 echo "--rx--"
 echo "diff /run/$testfile $blite/$testfile"
+echo
+echo "--- mytestspw in $libf ---"
+echo "mytestspw rx > winnie.log"
+echo "cp $blite/$testfile $libf"
+echo "testspw tx $testfile"
+echo "--rx--"
+echo "diff $libf/cp_$testfile $libf/$testfile"
 echo
